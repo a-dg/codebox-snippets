ced52ffada0808cd8a497d6be8e7b92ab9b1894c
Alan Dague-Greene <alan@basicrecipe.com>
2015-04-06 10:13:58 -0600

    Add dimensional properties to CSS-to-rem() replacement

--- snippets/convert-css-to-rem-function-by-property-with-units.sh
+++ snippets/convert-css-to-rem-function-by-property-with-units.sh
@@ -1,5 +1,5 @@
 # Find
-^(\s*(?:left|top|width|height|background|background-position|margin))\:\s*(?!(?:r|rr|rem|rem_round)\()(\S.*\d+p[xt].*);(\s*/{2,3}.*\S|\s*/\*.*\*/)?\s*$
+^(\s*(?:left|right|top|bottom|margin|margin-left|margin-right|margin-top|margin-bottom|padding|padding-left|padding-right|padding-top|padding-bottom|width|height|min-width|min-height|max-width|max-height|border|border-left|border-right|border-top|border-bottom|border-width|border-left-width|border-right-width|border-top-width|border-bottom-width|background-position|background-size|box-shadow|font-size|line-height|letter-spacing|word-spacing|text-indent|text-shadow|marker-offset))\:\s*(?!(?:r|rr|rem|rem_round)\()(\S.*\d+p[xt].*);(\s*/{2,3}.*\S|\s*/\*.*\*/)?\s*$
 
 # Replace
 $1: r($2);$3
