4348446a0c6c39df4c0571c6d7284a6616d2ce1f
Alan Dague-Greene <alan@basicrecipe.com>
2015-04-27 20:47:47 -0600

    Remove data attributes from obfuscated email address

--- snippets/obfuscate-email-address.js
+++ snippets/obfuscate-email-address.js
@@ -18,7 +18,14 @@
       + '@' + $email_link.attr(email_data_attr_prefix + '2')
       + '.' + $email_link.attr(email_data_attr_prefix + '3');
     var email_href = 'mailto:' + email_address;
-    $email_link.attr('href', email_href).removeClass(email_obfuscated_class).removeAttr(email_data_attr_prefix + '1').find('span').remove();
+    $email_link.attr('href', email_href)
+      .removeClass(email_obfuscated_class)
+      .removeAttr(
+        email_data_attr_prefix + '1'
+        + ' ' + email_data_attr_prefix + '2'
+        + ' ' + email_data_attr_prefix + '3'
+      )
+      .find('span').remove();
     
     // Prevent having to double-tap email link on mobile
     if(e.type === 'touchstart') {
