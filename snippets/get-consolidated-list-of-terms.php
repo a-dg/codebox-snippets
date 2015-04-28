3d19e2b627ef50d313d93ffbc73dd4f4fda99d73
Alan Dague-Greene <alan@basicrecipe.com>
2015-04-28 06:28:45 -0600

    Consolidate consolidated terms snippets

--- snippets/get-consolidated-list-of-terms.php
+++ snippets/get-consolidated-list-of-terms.php
@@ -1,6 +1,15 @@
-// display consolidated list of keywords used on research for this focus area
+// Display consolidated list of keywords used on research for this focus area
 $all_keywords = array();
 $consolidated_keywords = array();
+
+// Add to new array without unwanted keys
+// foreach($taxonomy as $tax) {
+  // object_id is always unique and prevents us from removing duplicates
+  // term_order is unused
+  // unset($tax->object_id);
+  // unset($tax->term_order);
+  // $tags[] = $tax;
+// }
 
 foreach($research_items as $ri) {
   $all_keywords[] = $ri->getTerms('keyword');
@@ -11,15 +20,21 @@ foreach($all_keywords as $post_keywords) {
   }
 }
 
-// can't just use array_unique because it's full of objects, i guess
-$consolidated_keywords = array_map('unserialize', array_unique(array_map('serialize', $consolidated_keywords)));
-
 if(Arr::iterable($consolidated_keywords)) {
-  echo '<h3>Keywords</h3><p>';
+  usort($consolidated_keywords, function($a, $b){
+    return strnatcasecmp($a->name, $b->name);
+  });
+  
+  // Serialized arrays are treated as strings to remove duplicates
+  $consolidated_keywords = array_map('unserialize', array_unique(array_map('serialize', $consolidated_keywords)));
+  
   $keyword_list = array();
   foreach($consolidated_keywords as $keyword) {
     $keyword_list[] = '<a href="'.URL_RESEARCH_DATABASE.'#'.$keyword->term_id.'">'.$keyword->name.'</a>';
   }
-  echo implode(', ', $keyword_list);
-  echo '</p>';
+  echo sprintf(
+    '<h3>Keywords</h3>
+    <p>%s</p>',
+    join(', ', $keyword_list)
+  );
 }
